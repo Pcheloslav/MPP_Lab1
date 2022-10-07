812e794ff56474b6ba51e1f480e12d182b32737d
Pcheloslav <slav.malink@gmail.com>
2022-10-07 19:29:27 +0300

    added xml, yaml serialization

---
+++ Tracer.Serialization/Tracer.Serialization.Xml/TraceResultSerializerXml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using Tracer.Serialization.Abstractions;
+
+namespace Tracer.Serialization.Xml
+{
+    public class XmlMethodTrace
+    {
+        [XmlAttribute("name")]
+        public string MethodName { get; }
+        [XmlAttribute("class")]
+        public string ClassName { get; }
+        [XmlAttribute("time")]
+        public string Time { get; }
+        [XmlElement("method")]
+        public List<XmlMethodTrace> Methods { get; }
+
+        public XmlMethodTrace(string methodName, string className, long time, List<XmlMethodTrace> methods)
+        {
+            MethodName = methodName;
+            ClassName = className;
+            Time = String.Format("{0}ms", time);
+            Methods = methods;
+        }
+
+        public static List<XmlMethodTrace> ToXmlMethods(IReadOnlyList<MethodTrace> methods)
+        {
+            if (methods.Count == 0)
+            {
+                return null;
+            }
+
+            List<XmlMethodTrace> traceMethods = new();
+            foreach (var method in methods)
+            {
+                traceMethods.Add(new XmlMethodTrace(method.MethodName, method.ClassName,
+                    method.Time, ToXmlMethods(method.Methods)));
+            }
+            return traceMethods;
+        }
+
+        public XmlMethodTrace()
+        {
+            MethodName = String.Empty;
+            ClassName = String.Empty;
+            Time = String.Empty;
+            Methods = new List<XmlMethodTrace>();
+        }
+
+    }
+
+    public class XmlThreadTrace
+    {
+        [XmlAttribute("id")]
+        public string Id { get; }
+        [XmlAttribute("time")]
+        public string Time { get; }
+        [XmlElement("method")]
+        public List<XmlMethodTrace> Methods { get; }
+
+        public XmlThreadTrace(int id, long time, List<XmlMethodTrace> methods)
+        {
+            Id = id.ToString();
+            Time = String.Format("{0}ms", time);
+            Methods = methods;
+        }
+        public XmlThreadTrace()
+        {
+            Id = String.Empty;
+            Time = String.Empty;
+            Methods = new List<XmlMethodTrace>();
+        }
+
+
+    }
+
+    [XmlRoot("root")]
+    public class XmlTraceResult
+    {
+        
+
+        [XmlElement("thread")]
+        public List<XmlThreadTrace> Threads { get; }
+        public XmlTraceResult(List<XmlThreadTrace> threads)
+        {
+            Threads = threads;
+        }
+        public XmlTraceResult()
+        {
+        }
+
+    }
+
+    public class TraceResultSerializerXml : ITraceResultSerializer
+    {
+        private string _format = "xml";
+        public string Format => _format;
+
+        public void Serialize(TraceResult traceResult, Stream to)
+        {
+            /*var options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+            };*/
+
+            List<XmlThreadTrace> threadTraces = new();
+            foreach (var thread in traceResult.Threads)
+            {
+                threadTraces.Add(new XmlThreadTrace(thread.Id, thread.Time,
+                    XmlMethodTrace.ToXmlMethods(thread.Methods)));
+            }
+            var res = new XmlTraceResult(threadTraces);
+            var xmlWriter = XmlWriter.Create(to, new XmlWriterSettings { Indent = true });
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(XmlTraceResult));
+            xmlSerializer.Serialize(xmlWriter, new XmlTraceResult(threadTraces));
+        }
+    }
+}
+
---
+++ Tracer.Serialization/Tracer.Serialization.Yaml/TraceResultSerializerYaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tracer.Serialization.Abstractions;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+
+namespace Tracer.Serialization.Yaml
+{
+    internal class YamlMethodTrace
+    {
+         public string MethodName { get; }
+         public string ClassName { get; }
+         public string Time { get; }
+         public List<YamlMethodTrace> Methods { get; }
+
+        internal YamlMethodTrace(string methodName, string className, long time, List<YamlMethodTrace> methods)
+        {
+            MethodName = methodName;
+            ClassName = className;
+            Time = String.Format("{0}ms", time);
+            Methods = methods;
+        }
+
+        internal static List<YamlMethodTrace> ToYamlMethods(IReadOnlyList<MethodTrace> methods)
+        {
+            if (methods.Count == 0)
+            {
+                return null;
+            }
+
+            List<YamlMethodTrace> traceMethods = new();
+            foreach (var method in methods)
+            {
+                traceMethods.Add(new YamlMethodTrace(method.MethodName, method.ClassName,
+                    method.Time, ToYamlMethods(method.Methods)));
+            }
+            return traceMethods;
+        }
+    }
+
+    internal class YamlThreadTrace
+    {
+         public string Id { get; }
+         public string Time { get; }
+         public List<YamlMethodTrace> Methods { get; }
+
+        internal YamlThreadTrace(int id, long time, List<YamlMethodTrace> methods)
+        {
+            Id = id.ToString();
+            Time = String.Format("{0}ms", time);
+            Methods = methods;
+        }
+    }
+
+    internal class YamlTraceResult
+    {
+         public List<YamlThreadTrace> Threads { get; }
+
+        public YamlTraceResult(List<YamlThreadTrace> threads)
+        {
+            Threads = threads;
+        }
+    }
+
+    public class TraceResultSerializerYaml : ITraceResultSerializer
+    {
+        private string _format = "Yaml";
+        public string Format => _format;
+
+        public void Serialize(TraceResult traceResult, Stream to)
+        {
+            /*var options = new YamlSerializerOptions
+            {
+                WriteIndented = true,
+                DefaultIgnoreCondition = YamlIgnoreCondition.WhenWritingNull
+            };*/
+
+            List<YamlThreadTrace> threadTraces = new();
+            foreach (var thread in traceResult.Threads)
+            {
+                threadTraces.Add(new YamlThreadTrace(thread.Id, thread.Time,
+                    YamlMethodTrace.ToYamlMethods(thread.Methods)));
+            }
+
+            var serializer = new SerializerBuilder()//.WithNamingConvention(CamelCaseNamingConvention.Instance)
+                           .Build();
+
+            var yaml = serializer.Serialize(new YamlTraceResult(threadTraces));
+            var sw = new StreamWriter(to);
+            sw.Write(yaml);
+            sw.Flush();
+            //YamlSerializer.Serialize(to, new YamlTraceResult(threadTraces), options);
+        }
+    }
+}
